0e6077e1fe6f010ca65c6d161463286765619b26
Hari Om Ranjan Srivastava <130528416+omsrivastavaa@users.noreply.github.com>
2024-02-13 17:52:07 +0000

    Conversion operation of datatypes

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let scoreinNumber = Number(score)
+//let scoreinString = String(score)
+//let scoreinBoolean = Boolean(score)
+
+console.log(typeof scoreinNumber);
+console.log(scoreinNumber);
+
+//33 => 33
+//"33abc" => NaN => Not a Number
+//Null => 0
+//Undefined => NaN
+//True/False => 1/0
