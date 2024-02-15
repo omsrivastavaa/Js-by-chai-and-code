791c2a04d1979fe2d56e86da03471c9d86dda4a9
Hari Om Ranjan Srivastava <130528416+omsrivastavaa@users.noreply.github.com>
2024-02-15 11:31:17 +0000

    Comparison in js

---
+++ .devcontainer/basics/comparison.js
@@ -0,0 +1,14 @@
+// // console.log("2" > 1);
+// // console.log("02" > 1);
+
+/* Avoid these type of conversion
+
+// console.log(null > 0 ); //false 
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+*/
+
+// === (Strict Check)
+
+console.log("2" === 2);
